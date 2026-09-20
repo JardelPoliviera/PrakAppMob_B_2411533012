b85e2c1ae5c1620b679afe2464af41239ad6e5d9
JardelPoliviera <jardelpoliviera@gmail.com>
2026-09-20 09:39:56 +0700

    Menambahkan tugas Praktikum Modul 1 dan Modul 2

---
+++ praktikum_pekan2_modul2/android/app/src/main/kotlin/com/example/praktikum_pekan2_modul2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.praktikum_pekan2_modul2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ praktikum_pekan_2_modul1/android/app/src/main/kotlin/com/example/praktikum_pekan_2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.praktikum_pekan_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
